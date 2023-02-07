99df065465072584295888ff2a77054667656d69
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-07 17:44:29 +0900

    #1. Django/DRF를 통한 API 구현 샘플

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,1 +1,12 @@
-urlpatterns = []
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(
+    "post", views.PostViewSet
+)  # 2개의 URL을 만들어줍니다. (router.urls에 리스트 형태로 존재합니다.)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+
+class PostViewSet(ModelViewSet):
+    # 최소 2가지를 해줘야 합니다.
+    queryset = Post.objects.all()  # 1. 데이터의 범위를 지정해줘야합니다.
+    serializer_class = PostSerializer  # 2. Serializer Class를 지정해줘야합니다.
